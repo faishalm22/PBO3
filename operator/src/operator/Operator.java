92b54207e1fb2daa2a43090b83f7b502238752b1
faishalm22 <77891212+faishalm22@users.noreply.github.com>
2021-09-24 22:36:23 +0700

    operator

---
+++ operator/src/operator/Operator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operator;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Operator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+        System.out.println(nx+" math round");
+    }
+}
+    
