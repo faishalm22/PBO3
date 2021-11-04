99c135aac44f0338a21a8b56a7b62a976556e297
faishalm22 <77891212+faishalm22@users.noreply.github.com>
2021-11-04 07:22:51 +0700

    week7

---
+++ WEEK7/task2/src/task2/Task2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task2;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Task2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	Shape shape = new Shape();
+	Circle circle = new Circle();
+	Rectangle rec = new Rectangle();
+	Square square = new Square();
+		
+	System.out.println(shape);
+	System.out.println(circle);
+	System.out.println(rec);
+	System.out.println(square);
+    }
+    
+}
